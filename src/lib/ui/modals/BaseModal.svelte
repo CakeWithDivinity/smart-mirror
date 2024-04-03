8996da962a5628532be78c7d4b2e0c5315c5b72f
hendrik.loewe <hendrikloewe3@gmail.com>
2024-04-03 20:24:05 +0200

    feat: add overlay to create new display SMA-3

---
+++ src/lib/ui/modals/BaseModal.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let isOpen: boolean;
+</script>
+
+{#if isOpen}
+	<div role="dialog" class="modal">
+		<div class="title"><slot name="title" /></div>
+		<div class="content"><slot name="content" /></div>
+		<div class="actions"><slot name="actions" /></div>
+	</div>
+{/if}
+
+<style lang="scss">
+	.modal {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 2rem;
+		background-color: var(--white);
+		padding-block: 1.5rem;
+		padding-inline: 3rem;
+		border-radius: 1rem;
+	}
+
+	.content {
+		min-width: 30rem;
+	}
+
+	.actions {
+		display: flex;
+		justify-content: end;
+		gap: 0.5rem;
+		width: 100%;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
 	import '../globals.scss';
+	import { Modals, closeModal } from 'svelte-modals';
 </script>
 
 <slot />
+
+<Modals>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div slot="backdrop" class="backdrop" on:click={closeModal}></div>
+</Modals>
+
+<style lang="scss">
+	.backdrop {
+		position: fixed;
+		inset: 0;
+		background-color: rgba(0, 0, 0, 0.8);
+	}
+</style>
