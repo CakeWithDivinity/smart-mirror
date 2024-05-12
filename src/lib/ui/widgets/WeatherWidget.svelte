95035dddac79390920426fe2d76ccaa8e54b0a3e
Lugril <lugril.m@gmail.com>
2024-05-12 23:12:44 +0200

    feat: Update WeatherWidget to show real-time weather data on api

--- src/lib/ui/widgets/WeatherWidget.svelte
+++ src/lib/ui/widgets/WeatherWidget.svelte
@@ -3,15 +3,15 @@
 	import { error } from '@sveltejs/kit';
 
 	export let widget: WeatherWidget;
-	async function getWeather(): Promise<any> {
+	async function getCurrent(apiKey: string, location: string): Promise<any> {
 		return await fetch(
-			'http://api.weatherapi.com/v1/current.json?key=ecc75202bc194e1da32140449241205&q=Lövenix&aqi=no',
+			`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3&aqi=no&alerts=no`,
 		)
 			.then((res) => {
 				if (res.status !== 200) {
 					console.log(res);
 				}
-					return res.json();	
+				return res.json();
 			})
 			.catch((error) => {
 				// Handle any errors that occur during the API call
@@ -20,27 +20,92 @@
 			});
 	}
 
-	export let response = getWeather();
+	export let apiKey = 'ecc75202bc194e1da32140449241205'; // Set your API key here
+	export let location = 'Lövenich'; // Set your location here
+
+	// Call the getCurrent function with the apiKey
+	export let response = getCurrent(apiKey, location);
 	console.log(response);
+
+	function getWeekday(is_day: Number) {
+		switch (is_day) {
+			case 0:
+				return 'Sonntag';
+			case 1:
+				return 'Montag';
+			case 2:
+				return 'Dienstag';
+			case 3:
+				return 'Mittwoch';
+			case 4:
+				return 'Donnerstag';
+			case 5:
+				return 'Freitag';
+			case 6:
+				return 'Samstag';
+			default:
+				return 'Kein gültiger Tag';
+		}
+	}
+
+	function getWeekdayAbbreviation(is_day: Number) {
+		switch (is_day) {
+			case 0:
+				return 'So';
+			case 1:
+				return 'Mo';
+			case 2:
+				return 'Di';
+			case 3:
+				return 'Mi';
+			case 4:
+				return 'Do';
+			case 5:
+				return 'Fr';
+			case 6:
+				return 'Sa';
+			default:
+				return 'Not a valid day';
+		}
+	}
 </script>
 
-{widget.type} Testwirget :) if api call return json, show it here
+{widget.type}
 
 {#if response}
 	{#await response}
 		<p>Loading...</p>
 	{:then weatherData}
 		{#if weatherData.current}
-			<h2>{weatherData.current.temp_c}°C</h2>
+			<p>{getWeekday(weatherData.current.is_day)}</p>
+			<!--
+					maybe add later on, problem: text is english
+					<p>{weatherData.current.condition.text}</p>
+				-->
+			<img src={weatherData.current.condition.icon} alt="Weather Icon" />
+		{/if}
+		{#if weatherData.forecast}
+			<p>
+				{weatherData.forecast.forecastday[0].day.mintemp_c}°C - {weatherData.forecast.forecastday[0]
+					.day.maxtemp_c}°C
+			</p>
+			<p>{weatherData.current.temp_c}°C</p>
+			<p>
+				{getWeekdayAbbreviation(weatherData.current.is_day + 1)}
+				<img src={weatherData.forecast.forecastday[1].day.condition.icon} alt="Weather Icon" />
+				{weatherData.forecast.forecastday[1].day.mintemp_c}°C - {weatherData.forecast.forecastday[1].day.maxtemp_c}°C
+			</p>
 		{/if}
 		{#if weatherData.location}
-			<h2>{weatherData.location.name}</h2>
+			<p>{new Date(weatherData.location.localtime).toLocaleDateString('de-DE')}</p>
 		{/if}
 		{#if weatherData.error}
 			<p>{weatherData.error.message}</p>
 		{/if}
+		<!--
+		error will never occure, lol
+		-->
 	{:catch error}
 		<p>Error: {error.message}</p>
 	{/await}
 {/if}
-
